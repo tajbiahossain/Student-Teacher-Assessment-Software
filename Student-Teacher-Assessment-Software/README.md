e1990e25ae8c0c7caa4e263725de39510b2c553a
unknown <tajbiahossain@gmail.com>
2020-03-06 01:04:50 +0600

    STA application

---
+++ Student-Teacher-Assessment-Software/README.md
@@ -0,0 +1,1 @@
+# Student-Teacher-Assessment-Software-CSE3224
---
+++ Student-Teacher-Assessment-Software/src/STA.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nowrin
+ */
+public class STA {
+
+    public static void main(String[] args) {
+        login ob = new login();
+        ob.setVisible(true);
+    }
+}
